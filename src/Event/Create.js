e1f3d06c7df40ba2597818823275a49a7aee072f
aizuanjeme <stephanieademuyiwa1@gmail.com>
2021-10-15 13:47:20 +0100

    mistake

--- src/Event/Create.js
+++ src/Event/Create.js
@@ -1,109 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
+// import { useForm } from 'react-hook-form'
 import './event.css';
 // import './events.css';
 
 const Create = () => {
-    return ( 
-        <div class="page-wrapper bg-gra-03 p-t-45 p-b-50">
-        <div class="wrapper wrapper--w790">
-            <div class="card card-5">
-                <div class="card-heading">
-                    <h2 class="title">Event Registration Form</h2>
-                </div>
-                <div class="card-body">
-                    <form method="POST">
-                        <div class="form-row m-b-55">
-                            <div class="name">Name</div>
-                            <div class="value">
-                                <div class="row row-space">
-                                    <div class="col-2">
-                                        <div class="input-group-desc">
-                                            <input class="input--style-5" type="text" name="first_name"/>
-                                            <label class="label--desc">first name</label>
-                                        </div>
-                                    </div>
-                                    <div class="col-2">
-                                        <div class="input-group-desc">
-                                            <input class="input--style-5" type="text" name="last_name"/>
-                                            <label class="label--desc">last name</label>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-row">
-                            <div class="name">Company</div>
-                            <div class="value">
-                                <div class="input-group">
-                                    <input class="input--style-5" type="text" name="company"/>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-row">
-                            <div class="name">Email</div>
-                            <div class="value">
-                                <div class="input-group">
-                                    <input class="input--style-5" type="email" name="email"/>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-row m-b-55">
-                            <div class="name">Phone</div>
-                            <div class="value">
-                                <div class="row row-refine">
-                                    <div class="col-3">
-                                        <div class="input-group-desc">
-                                            <input class="input--style-5" type="text" name="area_code"/>
-                                            <label class="label--desc">Area Code</label>
-                                        </div>
-                                    </div>
-                                    <div class="col-9">
-                                        <div class="input-group-desc">
-                                            <input class="input--style-5" type="text" name="phone"/>
-                                            <label class="label--desc">Phone Number</label>
-                                        </div>
+    // const { register, handleSubmit, errors, setValue, watch, trigger } = useForm({
+    //     mode: "onChange",
+    //     reValidateMode: 'onChange'
+    //   });
+    const [state, setState] = useState({
+        events: [],
+        event: {
+            name: "",
+            // phoneNo: "",
+            address: "",
+            startDate: null,
+            startTime: null,
+            // organizerName: "",
+            // organizerPhoneNumber: ""
+        }
+    })
+
+    const handleOnChange = (e) => {
+        setState({ event: e.target.value })
+    }
+
+    return (
+        <div className="page-wrapper bg-gra-03 p-t-45 p-b-50">
+            <div className="wrapper wrapper--w790">
+                <div className="card card-5">
+                    <div className="card-heading">
+                        <h2 className="title">Event Registration Form</h2>
+                    </div>
+                    <div className="card-body">
+                        <form method="POST">
+                            <div className="form-row m-b-55">
+                                <div className="name">Name of Event</div>
+                                <div className="value">
+                                    <div className="input-group">
+                                        <input className="input--style-5" type="text" name="name" />
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="form-row">
-                            <div class="name">Subject</div>
-                            <div class="value">
-                                <div class="input-group">
-                                    <div class="rs-select2 js-select-simple select--no-search">
-                                        <select name="subject">
-                                            <option disabled="disabled" selected="selected">Choose option</option>
-                                            <option>Subject 1</option>
-                                            <option>Subject 2</option>
-                                            <option>Subject 3</option>
-                                        </select>
-                                        <div class="select-dropdown"></div>
+                            <div className="form-row">
+                                <div className="name">Address</div>
+                                <div className="value">
+                                    <div className="input-group">
+                                        <input className="input--style-5" type="text" name="address" />
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="form-row p-t-20">
-                            <label class="label label--block">Are you an existing customer?</label>
-                            <div class="p-t-15">
-                                <label class="radio-container m-r-55">Yes
-                                    <input type="radio" checked="checked" name="exist"/>
-                                    <span class="checkmark"></span>
-                                </label>
-                                <label class="radio-container">No
-                                    <input type="radio" name="exist"/>
-                                    <span class="checkmark"></span>
-                                </label>
+
+                            <div className="form-row">
+                                <div className="name" htmlFor="p_startDate">Start Date</div>
+                                <div className="value">
+                                    <div className="input-group">
+                                        <input
+                                            type="date"
+                                            id="p_startDate"
+                                            className="form-control"
+                                            name="startDate"
+                                            placeholder="Poll startDate"
+                                            onChange={(e) => handleOnChange(e)}
+                                            // ref={register({ required: true })}
+                                        />
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div>
-                            <button class="btn btn--radius-2 btn--red" type="submit">Register</button>
-                        </div>
-                    </form>
+
+                            <div className="form-row">
+                                <div className="name" htmlFor='p_startTime'>Start Time</div>
+                                <div className="value">
+                                    <div className="input-group">
+                                        <input type="time"
+                                            id='p_startTime'
+                                            name="startTime"
+                                            className="form-control"
+                                            placeholder="Poll start time"
+                                            onChange={(e) => handleOnChange(e)}
+                                            // ref={register({ required: true })}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <button className="btn btn--radius-2 btn--red" type="submit">Register</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-     );
+    );
 }
- 
+
 export default Create;
