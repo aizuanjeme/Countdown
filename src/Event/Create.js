db2d3f8e93ab5e730f175858f21b5785cdc66942
aizuanjeme <stephanieademuyiwa1@gmail.com>
2022-03-25 10:08:06 +0100

    the event countdown for download

--- src/Event/Create.js
+++ src/Event/Create.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./event.css";
 import Counter from "../Shared/Counter";
+import ReactToPdf from "react-to-pdf";
 
 const getDatafromLS = () => {
   const data = localStorage.getItem("event");
@@ -65,137 +66,145 @@ const Create = (props) => {
     localStorage.setItem("event", json);
   }, [event]);
 
+  const ref = React.createRef();
+
   return (
-    <div className="page-wrapper bg-gra-03 p-t-45 p-b-50">
-      <div className="container">
-        <div class="row">
-          <h2 className="title">Event Registration Form</h2>
-          <div class="col-lg-4 col-md-5 col-12 align-self-center mb-3">
-            <div className="card mr-4">
-              <div className="card-body">
-                <form>
-                  <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">
-                      Name of Event
-                    </label>
-                    <input
-                      className="input--style-5"
-                      type="text"
-                      name="name"
-                      onChange={(e) => setName(e.target.value)}
-                      value={name}
-                    />
-                  </div>
+    <>
+      <div className="page-wrapper bg-gra-03 p-t-45 p-b-50">
+        <div className="container">
+          <div class="row">
+            <h2 className="title">Event Registration Form</h2>
+            <div class="col-lg-4 col-md-5 col-12 align-self-center mb-3">
+              <div className="card mr-4">
+                <div className="card-body">
+                  <form>
+                    <div class="mb-3">
+                      <label for="exampleFormControlInput1" class="form-label">
+                        Name of Event
+                      </label>
+                      <input
+                        className="input--style-5"
+                        type="text"
+                        name="name"
+                        onChange={(e) => setName(e.target.value)}
+                        value={name}
+                      />
+                    </div>
 
-                  <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">
-                      Address
-                    </label>
-                    <input
-                      className="input--style-5"
-                      type="text"
-                      name="address"
-                      onChange={(e) => setAddress(e.target.value)}
-                      value={address}
-                    />
-                  </div>
+                    <div class="mb-3">
+                      <label for="exampleFormControlInput1" class="form-label">
+                        Address
+                      </label>
+                      <input
+                        className="input--style-5"
+                        type="text"
+                        name="address"
+                        onChange={(e) => setAddress(e.target.value)}
+                        value={address}
+                      />
+                    </div>
 
-                  <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">
-                      Start Date
-                    </label>
-                    <input
-                      type="date"
-                      id="p_startDate"
-                      className="form-control"
-                      name="startDate"
-                      placeholder="Poll startDate"
-                      onChange={(e) => setStartDate(e.target.value)}
-                      value={startDate}
+                    <div class="mb-3">
+                      <label for="exampleFormControlInput1" class="form-label">
+                        Start Date
+                      </label>
+                      <input
+                        type="date"
+                        id="p_startDate"
+                        className="form-control"
+                        name="startDate"
+                        placeholder="Poll startDate"
+                        onChange={(e) => setStartDate(e.target.value)}
+                        value={startDate}
                       // ref={register({ required: true })}
-                    />
-                  </div>
+                      />
+                    </div>
 
-                  <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">
-                      Start Time
-                    </label>
-                    <input
-                      type="time"
-                      id="p_startTime"
-                      name="startTime"
-                      className="form-control"
-                      placeholder="Poll start time"
-                      onChange={(e) => setStartTime(e.target.value)}
-                      value={startTime}
+                    <div class="mb-3">
+                      <label for="exampleFormControlInput1" class="form-label">
+                        Start Time
+                      </label>
+                      <input
+                        type="time"
+                        id="p_startTime"
+                        name="startTime"
+                        className="form-control"
+                        placeholder="Poll start time"
+                        onChange={(e) => setStartTime(e.target.value)}
+                        value={startTime}
                       // ref={register({ required: true })}
-                    />
-                  </div>
+                      />
+                    </div>
 
-                  <button
-                    className="btn btn--radius-2 btn--red"
-                    type="submit"
-                    onClick={addEvent}
-                  >
-                    Save
-                  </button>
-                </form>
+                    <button
+                      className="btn btn--radius-2 btn--red"
+                      type="submit"
+                      onClick={addEvent}
+                    >
+                      Save
+                    </button>
+                  </form>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="col-lg-8 col-md-6 col-12 align-self-start">
-            <div className="container bg-white">
-              {event &&
-                event.length > 0 &&
-                event.map((event) => (
-                  <div className="container border-bottom p-3">
-                    <div className="row">
-                      <div className="col-9">
-                        <h4>{event.name}</h4>
-                        <span>{event.address}</span>
-                        <br />
-                        <span>
-                          {event.startDate} &nbsp; {event.startTime}
-                        </span>
-                      </div>
-                      <div className="col-3">
-                        <div>
-                          <i
-                            class="fas fa-trash-alt text-danger"
-                            onClick={() => deleteEvent(event.id)}
-                            title="delete"
-                          ></i>
-                          &nbsp;
-                          <i
-                            class="fas fa-edit text-warning"
-                            onClick={() => editEvent(event.id)}
-                            title="edit"
-                          ></i>
+            <div class="col-lg-8 col-md-6 col-12 align-self-start" >
+              <ReactToPdf targetRef={ref} filename="div-blue.pdf">
+                {({ toPdf }) => (
+                  <button className="btn btn-primary" onClick={toPdf}>Generate pdf</button>
+                )}
+              </ReactToPdf>
+              <div className="container bg-white" ref={ref} >
+                {event &&
+                  event.length > 0 &&
+                  event.map((event) => (
+                    <div className="container border-bottom p-3"  >
+                      <div className="row">
+                        <div className="col-9"  >
+                          <h4>{event.name}</h4>
+                          <span>{event.address}</span>
+                          <br />
+                          <span>
+                            {event.startDate} &nbsp; {event.startTime}
+                          </span>
                         </div>
-                        <div></div>
+                        <div className="col-3">
+                          <div>
+                            <i
+                              class="fas fa-trash-alt text-danger"
+                              onClick={() => deleteEvent(event.id)}
+                              title="delete"
+                            ></i>
+                            &nbsp;
+                            <i
+                              class="fas fa-edit text-warning"
+                              onClick={() => editEvent(event.id)}
+                              title="edit"
+                            ></i>
+                          </div>
+                          <div></div>
+                        </div>
                       </div>
+                      {isBegin(event) && (
+                        <div>
+                          <Counter
+                            startDate={event.startDate}
+                            startTime={event.startTime}
+                          />
+                        </div>
+                      )}
                     </div>
-                    {isBegin(event) && (
-                      <div>
-                        <Counter
-                          startDate={event.startDate}
-                          startTime={event.startTime}
-                        />
-                      </div>
-                    )}
+                  ))}
+                {event?.length === 0 && (
+                  <div className="row">
+                    <strong className="mx-auto">No Event Found</strong>
                   </div>
-                ))}
-
-              {event?.length === 0 && (
-                <div className="row">
-                  <strong className="mx-auto">No Event Found</strong>
-                </div>
-              )}
+                )}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
